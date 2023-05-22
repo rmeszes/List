f0841b8a4f09943a9756e2ccc0abd5c06f9b7ae6
Meszes Richárd <79580855+rmeszes@users.noreply.github.com>
2023-05-22 20:14:38 +0200

    revamp done, push_back, push_front, empty, layered existance works, it has sentinels now, and is double linked

--- test.cpp
+++ test.cpp
@@ -5,10 +5,10 @@
 
 struct yeet {
 	List<int> list;
-	yeet() :list(List<int>()) {
-		list.Add(1);
-		list.Add(2);
-		list.Add(3);
+	yeet() {
+		list.push_back(1);
+		list.push_back(2);
+		list.push_back(3);
 	}
 	yeet(const yeet& y) :list(y.list){
 	}
@@ -22,8 +22,10 @@ struct yeet {
 };
 int main() {
 	
-	List<yeet> l3;
-	l3.Add(yeet());
-	l3.Add(yeet());
-	l3.Add(yeet());
+	List<yeet> l1;
+	l1.push_back(yeet());
+	l1.push_back(yeet());
+	l1.push_back(yeet());
+
+	std::list<int> l2;
 }
